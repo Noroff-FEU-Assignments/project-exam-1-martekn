54e62e88ecea5cb37f662ca6168a00538b30bd11
Marte <marte.mk@hotmail.com>
2023-03-12 09:03:07 +0000

    Add eventlisteners, functionality and further styling to carousel

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,3 +1,109 @@
+const carouselRightButton = document.querySelector("#carousel-right");
+const carouselLeftButton = document.querySelector("#carousel-left");
+const ul = document.querySelector("#carousel");
+
+let carouselWidth = ul.clientWidth;
+const cardWidth = ul.querySelector("li").clientWidth;
+const visibleCards = Math.floor(carouselWidth / cardWidth);
+const padding = (carouselWidth - cardWidth * visibleCards) / (visibleCards - 1);
+const dots = Array.from(document.querySelectorAll(".dots .dot"));
+
+let scrollX = 0;
+let dotCount = 0;
+let sliderIndex = 0;
+
+dots[sliderIndex].classList.add("active");
+
+const setDotCount = () => {
+  if (!window.matchMedia("(max-width: 70em)").matches) {
+    dotCount = 3;
+  } else if (!window.matchMedia("(max-width: 55em)").matches) {
+    dotCount = 4;
+  } else {
+    dotCount = dots.length;
+  }
+};
+
+const updateCarousel = (e) => {
+  carouselWidth = ul.clientWidth;
+  dots[sliderIndex].classList.remove("active");
+
+  if (e.target === carouselLeftButton) {
+    sliderIndex--;
+    if (sliderIndex < 0) {
+      sliderIndex = dotCount - 1;
+    }
+
+    if (scrollX <= 0) {
+      scrollX = ul.scrollLeftMax + carouselWidth + padding;
+    }
+
+    scrollX -= carouselWidth + padding;
+  } else if (e.target === carouselRightButton) {
+    sliderIndex++;
+    if (sliderIndex > dotCount - 1) {
+      sliderIndex = 0;
+    }
+
+    if (scrollX >= ul.scrollLeftMax) {
+      scrollX = 0 - carouselWidth - padding;
+    }
+
+    scrollX += carouselWidth + padding;
+  } else {
+    dots[sliderIndex].classList.add("active");
+    return;
+  }
+
+  e.target.disabled = true;
+  setTimeout(() => {
+    e.target.disabled = false;
+  }, 150);
+
+  ul.scroll({
+    left: scrollX,
+    top: 0,
+    behavior: "smooth",
+  });
+
+  dots[sliderIndex].classList.add("active");
+};
+
+const setupCarouselFunctionality = () => {
+  ul.scrollLeft = scrollX;
+  setDotCount();
+  carouselLeftButton.addEventListener("click", updateCarousel);
+  carouselRightButton.addEventListener("click", updateCarousel);
+
+  let timeout;
+  const resizeObserver = new ResizeObserver((entries) => {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = 0;
+    }
+
+    timeout = setTimeout(() => {
+      let width = entries[0].contentRect.width;
+
+      if (width !== carouselWidth) {
+        setDotCount();
+        dots[sliderIndex].classList.remove("active");
+        sliderIndex = 0;
+        dots[sliderIndex].classList.add("active");
+        scrollX = 0;
+
+        ul.scroll({
+          left: scrollX,
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+    }, 500);
+  });
+
+  resizeObserver.observe(ul);
+};
+
 export const setupIndex = () => {
-  // Page specific setup
+  setupCarouselFunctionality();
 };
