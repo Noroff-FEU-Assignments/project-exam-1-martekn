61a944d2f90cda3ee0fb8d17d314f6f3ab2f624c
Marte <marte.mk@hotmail.com>
2023-03-14 17:42:05 +0000

    Fetch data from api to populate hero section

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,3 +1,18 @@
+import { fetchApiResults } from "../util/api.js";
+
+const heroSection = document.querySelector("#about-hero");
+
+const createHero = async () => {
+  try {
+    const heroRes = await fetchApiResults("/wp/v2/pages", "?slug=about");
+    heroSection.innerHTML += heroRes[0].content.rendered;
+    heroSection.querySelector(".loader").remove();
+    console.log(heroRes[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const setupAbout = () => {
-  // Page specific setup
+  createHero();
 };
